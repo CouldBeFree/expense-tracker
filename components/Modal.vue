14f6fda4618ebf8e2c6a651228c61149d1ea6f6b
CouldBeFree <propokerstar90@gmail.com>
2019-08-12 21:38:31 +0300

    form

--- components/Modal.vue
+++ components/Modal.vue
@@ -9,31 +9,31 @@
           <v-layout wrap>
             <v-flex xs6>
               <v-text-field
-                label="Insert value"
-                value="00.00"
-                prefix="$"
-                @input="onInput"
-                prepend-icon="mdi-calculator"
+                      label="Insert value"
+                      type="number"
+                      v-model="amount"
+                      prefix="$"
+                      prepend-icon="mdi-calculator"
               ></v-text-field>
             </v-flex>
             <v-flex xs6>
               <v-menu
-                ref="menu"
-                v-model="menu"
-                :close-on-content-click="false"
-                :return-value.sync="date"
-                transition="scale-transition"
-                offset-y
-                full-width
-                min-width="290px"
+                      ref="menu"
+                      v-model="menu"
+                      :close-on-content-click="false"
+                      :return-value.sync="date"
+                      transition="scale-transition"
+                      offset-y
+                      full-width
+                      min-width="290px"
               >
                 <template v-slot:activator="{ on }">
                   <v-text-field
-                    v-model="date"
-                    label="Picker in menu"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-on="on"
+                          v-model="date"
+                          label="Picker in menu"
+                          prepend-icon="mdi-calendar"
+                          readonly
+                          v-on="on"
                   ></v-text-field>
                 </template>
                 <v-date-picker v-model="date" no-title scrollable>
@@ -45,18 +45,18 @@
             </v-flex>
             <v-flex xs6>
               <v-text-field
-                type="text"
-                label="Description"
-                prepend-icon="mdi-comment-text"
-                v-model="description"
+                      type="text"
+                      label="Description"
+                      prepend-icon="mdi-comment-text"
+                      v-model="description"
               ></v-text-field>
             </v-flex>
             <v-flex xs6>
               <v-select
-                :items="items"
-                label="Category"
-                prepend-icon="mdi-format-list-bulleted"
-                v-model="category"
+                      :items="items"
+                      label="Category"
+                      prepend-icon="mdi-format-list-bulleted"
+                      v-model="category"
               ></v-select>
             </v-flex>
           </v-layout>
@@ -64,42 +64,40 @@
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="$emit('input', false)">Close</v-btn>
-        <v-btn color="blue darken-1" text @click="setData">Save</v-btn>
+        <v-btn color="blue darken-1" @click="$emit('input', false)">Close</v-btn>
+        <v-btn color="blue darken-1" :disabled="!amount" @click="setData">Save</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-	export default {
-		name: "Modal",
-		props: ['value', 'label'],
-		data(){
-			return{
-				description: '',
-				amount: 0,
-				date: new Date().toISOString().substr(0, 10),
-				menu: false,
-				category: '',
-				items: ['Clothing', 'Food', 'Health Care', 'Home', 'Payments', 'Recreation']
+  export default {
+    name: "Modal",
+    props: ['value', 'label'],
+    data(){
+      return{
+        description: '',
+        amount: 0,
+        date: new Date().toISOString().substr(0, 10),
+        menu: false,
+        category: 'Clothing',
+        items: ['Clothing', 'Food', 'Health Care', 'Home', 'Payments', 'Recreation']
       }
     },
-		methods: {
-			onInput(val){
-				this.amount = val;
-      },
-			setData(){
+    methods: {
+      setData(){
         let data = {
-	        description: this.description,
-	        amount: this.amount,
+          description: this.description,
+          amount: this.amount,
           date: this.date,
           category: this.category
         };
-				this.$emit('input', false);
-			}
-		}
-	}
+        this.$refs.form.reset();
+        this.$emit('input', false);
+      }
+    }
+  }
 </script>
 
 <style scoped>
