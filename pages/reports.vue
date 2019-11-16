3c4f1fcfd4fa8a8d0fed9df36acad474e22eadf6
CouldBeFree <propokerstar90@gmail.com>
2019-11-16 22:12:00 +0200

    Finished reports

--- pages/reports.vue
+++ pages/reports.vue
@@ -2,7 +2,7 @@
   <v-container grid-list-lg grid-list-md grid-list-xs>
     <v-card class='pt-4'>
       <v-layout justify-center>
-        <date-picker></date-picker>
+        <date-picker @date="onDateSelect"></date-picker>
       </v-layout>
       <v-layout>
         <v-flex xs12>
@@ -10,18 +10,20 @@
             <v-layout>
               <v-flex xs8>
                 <pie-card
+                  v-if="refactoredData.length"
                   label='expense'
-                  :options='expense'
+                  :options='expenses'
                 ></pie-card>
               </v-flex>
               <v-flex xs4>
-                <!--<expenses-list
+                <expenses-list
+                  v-if="refactoredData.length"
                   v-for='(item, i) in refactoredData'
                   :category='item.category'
                   :amount='item.amount'
                   :percent='percentCount(item.amount)'
                   :key='i'
-                ></expenses-list>-->
+                ></expenses-list>
               </v-flex>
             </v-layout>
           </div>
@@ -42,6 +44,8 @@
   import ExpensesList from '../components/ExpensesList';
   import DatePicker from "../components/DatePicker";
 
+  import { mapMutations, mapState, mapActions } from 'vuex'
+
   export default {
     middleware: 'auth',
     name: 'reports',
@@ -52,13 +56,20 @@
     },
     data(){
       return {
-        isOpen: false
+        isOpen: false,
+        test: true
       }
     },
     computed: {
-      /*refactoredData: function () {
+      ...mapState('expenses', {
+        expenses: state => state.expenses
+      }),
+      ...mapState('common', {
+        total: state => state.total
+      }),
+      refactoredData: function () {
         let holder = {};
-        this.expense.forEach(el => {
+        this.expenses.forEach(el => {
           if (holder.hasOwnProperty(el.category)) {
             holder[el.category] = holder[el.category] + +el.amount;
           } else {
@@ -71,12 +82,23 @@
           target.push({ category: prop, amount: holder[prop] });
         }
         return target;
-      }*/
+      }
     },
     methods: {
       percentCount(amount){
-        return ((amount/this.count.expenses) * 100).toFixed(2)
+        return ((amount/this.total.expenses) * 100).toFixed(2)
+      },
+      ...mapActions('common', ['getTotal']),
+      ...mapActions('expenses', ['getExpenses']),
+      ...mapMutations('common', ['setDate']),
+      async onDateSelect(val){
+        this.setDate(`${val}-01`);
+        await this.getExpenses();
+        await this.getTotal();
       }
+    },
+    mounted(){
+      this.getTotal();
     }
   }
 </script>
